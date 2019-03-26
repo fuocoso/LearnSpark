200c1a31664b43fdc7cc7cab35aa109c19de9a85
fuocoso <35402721+fuocoso@users.noreply.github.com>
2019-03-26 10:03:28 +0800

    Add files via upload

---
+++ src/main/scala/com/spark/kafka/UserClickCountAnalytics.scala
@@ -0,0 +1,50 @@
+package com.spark.demo
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka.KafkaUtils
+import kafka.serializer.StringDecoder
+import com.alibaba.fastjson.JSON
+
+import org.apache.spark.streaming.dstream.InputDStream
+
+
+object UserClickCountAnalytics {
+  def main(args: Array[String]): Unit = {
+    
+
+    // Create a StreamingContext with the given master URL
+    val conf = new SparkConf().setMaster("local[2]").setAppName("UserClickCountStat")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    // Kafka configurations
+    val topics = Set(s"${KafkaProperties.KAFKA_USER_TOPIC}")
+    println(s"Topics: ${topics}.")
+
+    val brokers = KafkaProperties.KAFKA_ADDR
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> brokers,
+      "auto.offset.reset"-> "smallest"
+    )
+
+    //val clickHashKey = "app::users::click"
+
+    // Create a direct stream
+    val kafkaStream: InputDStream[(String, String)] = KafkaUtils
+        .createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+
+    val events = kafkaStream.flatMap(line => {
+      println(s"Line ${line}.")
+      val data = JSON.parseObject(line._2)
+      Some(data)
+    })
+
+    // Compute user click times
+    val userClicks = events.map(x => (x.getString("uid"), x.getLong("click_count"))).reduceByKey(_ + _)
+    userClicks.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
